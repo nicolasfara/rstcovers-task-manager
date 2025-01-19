1bb1222e2d98987810fe8c053aa70f76b4495031
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-01-19 18:04:31 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,177 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
+plugins {
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.gitSemVer)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotest.multiplatform)
+    alias(libs.plugins.kotlin.qa)
+    alias(libs.plugins.npm.publish)
+    alias(libs.plugins.multiJvmTesting)
+    alias(libs.plugins.publishOnCentral)
+    alias(libs.plugins.taskTree)
+}
+
+group = "org.danilopianini"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+multiJvm {
+    jvmVersionForCompilation.set(21)
+}
+
+kotlin {
+    jvmToolchain(21)
+
+    jvm {
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget = JvmTarget.JVM_1_8
+                }
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting { }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.bundles.kotlin.testing.common)
+                implementation(libs.bundles.kotest.common)
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(libs.kotest.runner.junit5)
+            }
+        }
+    }
+
+    js(IR) {
+        browser()
+        nodejs()
+        binaries.library()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
+        binaries.library()
+    }
+
+    // Temporarily disabled due to https://youtrack.jetbrains.com/issue/KT-72858
+//    wasmWasi {
+//        nodejs()
+//        binaries.library()
+//    }
+
+    val nativeSetup: KotlinNativeTarget.() -> Unit = {
+        binaries {
+            sharedLib()
+            staticLib()
+        }
+    }
+
+    applyDefaultHierarchyTemplate()
+    /*
+     * Linux 64
+     */
+    linuxX64(nativeSetup)
+    linuxArm64(nativeSetup)
+    /*
+     * Win 64
+     */
+    mingwX64(nativeSetup)
+    /*
+     * Apple OSs
+     */
+    macosX64(nativeSetup)
+    macosArm64(nativeSetup)
+    iosArm64(nativeSetup)
+    iosSimulatorArm64(nativeSetup)
+    watchosArm32(nativeSetup)
+    watchosArm64(nativeSetup)
+    watchosSimulatorArm64(nativeSetup)
+    tvosArm64(nativeSetup)
+    tvosSimulatorArm64(nativeSetup)
+
+    targets.all {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    allWarningsAsErrors = true
+                    freeCompilerArgs.add("-Xexpect-actual-classes")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    if (System.getenv("CI") == "true") {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+}
+
+publishOnCentral {
+    repoOwner.set("DanySK")
+    projectLongName.set("Template for Kotlin Multiplatform Project")
+    projectDescription.set("A template repository for Kotlin Multiplatform projects")
+    repository("https://maven.pkg.github.com/danysk/${rootProject.name}".lowercase()) {
+        user.set("DanySK")
+        password.set(System.getenv("GITHUB_TOKEN"))
+    }
+    publishing {
+        publications {
+            withType<MavenPublication> {
+                pom {
+                    developers {
+                        developer {
+                            name.set("Danilo Pianini")
+                            email.set("danilo.pianini@gmail.com")
+                            url.set("http://www.danilopianini.org/")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+npmPublish {
+    registries {
+        register("npmjs") {
+            uri.set("https://registry.npmjs.org")
+            val npmToken: String? by project
+            authToken.set(npmToken)
+            dry.set(npmToken.isNullOrBlank())
+        }
+    }
+}
+
+// Workaround for https://github.com/kotest/kotest/issues/4521 (fixed but not released)
+tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    compilerOptions {
+        allWarningsAsErrors = !name.contains("test", ignoreCase = true)
+    }
+}
+
+// Workaround for https://github.com/kotest/kotest/issues/4647
+val kotestBrokenTasks = listOf("wasmJsBrowserTest", "wasmJsD8Test")
+tasks.matching { it.name in kotestBrokenTasks }.configureEach {
+    enabled = false
+}
